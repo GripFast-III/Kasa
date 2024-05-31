f57e8a99f224fa214c9beccaf94ff94b830f82b7
GripFast-III <jeandbdp@gmail.com>
2024-05-31 21:44:12 +0200

    Ajouts dossiers + logo (test)

---
+++ projet-kasa/src/App.jsx
@@ -0,0 +1,48 @@
+/*import Hello from "./Components/hello";
+import Footer from "./Components/footer";
+import "./Styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Hello name="Roger" />
+        <Hello name="tata" />
+      </header>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+*/
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./Styles/main.scss";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer/index";
+
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Housing from "./Pages/Housing";
+import Error from "./Pages/Error";
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/housing/:housingId" element={<Housing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
---
+++ projet-kasa/src/Components/Header/index.jsx
@@ -0,0 +1,22 @@
+import { NavLink, Link } from 'react-router-dom';
+import logo from './../../Assets/LOGO.png';
+
+const Header = () => {
+    return (
+        <header className="header" role="banner">
+            <Link to="/">
+                <img className="header__logo" src={logo} alt="logo" />
+            </Link>
+            <nav className="header__nav">
+                <NavLink to="/" className="header__link">
+                    Accueil
+                </NavLink>
+                <NavLink to="/about" className="header__link">
+                    À propos
+                </NavLink>
+            </nav>
+        </header>
+    );
+    };
+
+    export default Header;
