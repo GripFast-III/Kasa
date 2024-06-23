22d31b45566cf8a46a8cd4cee03f895fad4dfece
GripFast-III <jeandbdp@gmail.com>
2024-06-24 01:45:12 +0200

    Modification photo banner about; changement css en scss; 0 erreur dans la console; site fonctionnel et conforme à la maquette

--- projet-kasa/src/App.jsx
+++ projet-kasa/src/App.jsx
@@ -1,29 +1,6 @@
-/*import React from "react"
-//import Hello from "./Components/hello";
-import Header from "./Components/Header";
-import Banner from "./Components/Banner/banner";
-import Gallery from "./Components/Gallery/index"
-import Footer from "./Components/Footer/footer";
-
-import "./Styles/App.css";
-
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Banner />
-      <Gallery />
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
-*/
-
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import "./Styles/main.scss";
+//import "./Styles/main.scss";
 import Loader from "./Components/Loader";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer/index";
@@ -37,10 +14,10 @@ const App = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Simuler un délai de chargement
+    // Loader de chargement
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000); // Changez la durée selon vos besoins
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
